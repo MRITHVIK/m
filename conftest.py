d2a58fb89a083c927c1f709e7a17abf9c35fd409
Mittai Rithvik <mittai_rithvik@epam.com>
2023-10-08 19:38:16 +0530

    new test

--- conftest.py
+++ conftest.py
@@ -2,8 +2,17 @@ import pytest
 from selenium import webdriver
 
 @pytest.fixture
-def test_apple():
-    driver = webdriver.Chrome()
-    aa= driver.get('https://www.apple.com/in/')
-    print("am apple")
-    pass
+def test_fixture1(request):
+    calling_test_name = request.node.name
+    if calling_test_name == 'test_google':
+     print(f"hi am {calling_test_name}")
+     driver = webdriver.Chrome()
+     driver.get('https://www.google.com/')
+    if calling_test_name == 'test_facebook':
+     print(f"hi am {calling_test_name}")
+     driver = webdriver.Chrome()
+     driver.get('https://www.facebook.com/')
+    if calling_test_name == 'test_apple':
+     print(f"hi am {calling_test_name}")
+     driver = webdriver.Chrome()
+     driver.get('https://www.apple.com/in/')
